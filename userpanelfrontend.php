baf8b8134664c6534c9dc3846c28b765e73f1745
nasanchez7 <nadirblanco02@gmail.com>
2024-01-11 16:59:38 -0300

    add license GPL

--- userpanelfrontend.php
+++ userpanelfrontend.php
@@ -5,12 +5,31 @@
 /*
 Plugin Name: User Panel Frontend
 Plugin URI https://nadirsanchez.com.ar
-Description: Crea usuarios y visualiza su informacion facilmente desde el frontend de tu pagina web, a traves de un simple shortcode.
+Description: Create users and view their information easily from the frontend of your website, through a simple shortcode.
 Version: 1.0.0
 Author: Nadir Sanchez
 Author URI: https://nadirsanchez.com.ar
 License: GPLv2 or later
 Text Domain: userpanelfrontend
+*/
+
+/*
+Create users and view their information easily from the frontend of your website, through a simple shortcode.
+Copyright (C) 2024  Nadir Sanchez
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 if(!defined('ABSPATH')){
